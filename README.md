780f7116a48df9f3ec5b4013390aeeb38d9b101b
Frank P. Moley III <frankmoley@gmail.com>
2018-09-01 11:19:22 -0500

    Incramental changes

---
+++ README.md
@@ -0,0 +1,12 @@
+# Kata Boot Actuator
+A project to show the feature sets of Spring Boot Actuator in Spring Boot 2.0
+
+## URLs to Evaluate
+http://localhost:8080/actuator
+
+http://localhost:8080/actuator/info
+
+http://localhost:8080/actuator/health
+
+http://localhost:8080/actuator/metrics/http.server.requests
+
--- src/main/java/com/frankmoley/kata/KataBootActuatorApplication.java
+++ src/main/java/com/frankmoley/kata/KataBootActuatorApplication.java
@@ -11,10 +11,9 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class KataBootActuatorApplication {
@@ -28,20 +27,38 @@ public class KataBootActuatorApplication {
     @RequestMapping("api")
     public class ApiController{
 
+        private final UserRepository userRepository;
+
+        public ApiController(UserRepository userRepository){
+            this.userRepository = userRepository;
+        }
+
         @RequestMapping("greeting")
         public String getGreeting(){
             return "Hello World";
+        }
+
+        @RequestMapping("users")
+        public List<User> getAllUsers(){
+            List<User> users = new ArrayList<>();
+            this.userRepository.findAll().forEach(user -> users.add(user));
+            return users;
         }
     }
 
     @Data
     @Entity
+    @Table(name="USER")
     public class User{
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
+        @Column(name="USER_ID")
         private long id;
+        @Column(name="FIRST_NAME")
         private String firstName;
+        @Column(name="LAST_NAME")
         private String lastName;
+        @Column(name="EMAIL_ADDRESS")
         private String emailAddress;
     }
 
